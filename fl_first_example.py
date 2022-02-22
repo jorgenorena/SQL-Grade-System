191cedc7e87f65fb474cca7a7a7f5648c620e9f1
jorge.norena <jorge.norena@gmail.com>
2022-02-22 10:43:32 -0300

    Add code for simple  federated learning example

--- fl_first_example.py
+++ fl_first_example.py
@@ -46,3 +46,209 @@ def load(paths, verbose=-1):
             print("[INFO] processed {}/{}".format(i + 1, len(paths)))
     # return a tuple of the data and labels
     return data, labels
+
+#declare path to your mnist data folder
+img_path = '/Users/jorgenorena/Dropbox/Academic 2022/Empresa/Federated Learning/FL_games/images/trainingSet/trainingSet'
+
+#get the path list using the path object
+image_paths = list(paths.list_images(img_path))
+
+#apply our function
+image_list, label_list = load(image_paths, verbose=10000)
+
+#binarize the labels
+lb = LabelBinarizer()
+label_list = lb.fit_transform(label_list)
+
+#split data into training and test set
+X_train, X_test, y_train, y_test = train_test_split(image_list, 
+                                                    label_list, 
+                                                    test_size=0.1, 
+                                                    random_state=42)
+# %%
+
+# Create the clients for simulation
+
+def create_clients(image_list, label_list, num_clients=10, initial='clients'):
+    ''' return: a dictionary with keys clients' names and value as 
+                data shards - tuple of images and label lists.
+        args: 
+            image_list: a list of numpy arrays of training images
+            label_list:a list of binarized labels for each image
+            num_client: number of fedrated members (clients)
+            initials: the clients'name prefix, e.g, clients_1 
+            
+    '''
+
+    #create a list of client names
+    client_names = ['{}_{}'.format(initial, i+1) for i in range(num_clients)]
+
+    #randomize the data
+    data = list(zip(image_list, label_list))
+    random.shuffle(data)
+
+    #shard data and place at each client
+    size = len(data)//num_clients
+    shards = [data[i:i + size] for i in range(0, size*num_clients, size)]
+
+    #number of clients must equal number of shards
+    assert(len(shards) == len(client_names))
+
+    return {client_names[i] : shards[i] for i in range(len(client_names))} 
+
+
+#create clients
+clients = create_clients(X_train, y_train, num_clients=10, initial='client')
+
+# %%
+
+# Create batches for processing in tensorflow
+
+def batch_data(data_shard, bs=32):
+    '''Takes in a clients data shard and create a tfds object off it
+    args:
+        shard: a data, label constituting a client's data shard
+        bs:batch size
+    return:
+        tfds object'''
+    #seperate shard into data and labels lists
+    data, label = zip(*data_shard)
+    dataset = tf.data.Dataset.from_tensor_slices((list(data), list(label)))
+    return dataset.shuffle(len(label)).batch(bs)
+
+
+#process and batch the training data for each client
+clients_batched = dict()
+for (client_name, data) in clients.items():
+    clients_batched[client_name] = batch_data(data)
+    
+#process and batch the test set  
+test_batched = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(len(y_test))
+# %%
+
+# Define the model
+
+class SimpleMLP:
+    @staticmethod
+    def build(shape, classes):
+        model = Sequential()
+        model.add(Dense(200, input_shape=(shape,)))
+        model.add(Activation("relu"))
+        model.add(Dense(200))
+        model.add(Activation("relu"))
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+        return model
+
+lr = 0.01 
+# Number of global epochs (aggregations among clients)
+# The learning rate will decay with the number of aggregations
+comms_round = 100
+loss='categorical_crossentropy'
+metrics = ['accuracy']
+optimizer = SGD(learining_rate=lr, 
+                decay=lr / comms_round, 
+                momentum=0.9
+               )
+
+
+# %%
+
+# Model aggregation (Federated averaging)
+
+# This function is unrealistic in a real world application
+def weight_scalling_factor(clients_trn_data, client_name):
+    client_names = list(clients_trn_data.keys())
+    #get the bs (batch size)
+    bs = list(clients_trn_data[client_name])[0][0].shape[0]
+    #first calculate the total training data points across clinets
+    global_count = sum([tf.data.experimental.cardinality(clients_trn_data[client_name]).numpy() for client_name in client_names])*bs
+    # get the total number of data points held by a client
+    local_count = tf.data.experimental.cardinality(clients_trn_data[client_name]).numpy()*bs
+    return local_count/global_count
+
+
+def scale_model_weights(weight, scalar):
+    '''function for scaling a models weights'''
+    weight_final = []
+    steps = len(weight)
+    for i in range(steps):
+        weight_final.append(scalar * weight[i])
+    return weight_final
+
+
+
+def sum_scaled_weights(scaled_weight_list):
+    '''Return the sum of the listed scaled weights. The is equivalent to scaled avg of the weights'''
+    avg_grad = list()
+    #get the average grad accross all client gradients
+    for grad_list_tuple in zip(*scaled_weight_list):
+        layer_mean = tf.math.reduce_sum(grad_list_tuple, axis=0)
+        avg_grad.append(layer_mean)
+        
+    return avg_grad
+
+
+def test_model(X_test, Y_test,  model, comm_round):
+    cce = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    #logits = model.predict(X_test, batch_size=100)
+    logits = model.predict(X_test)
+    loss = cce(Y_test, logits)
+    acc = accuracy_score(tf.argmax(logits, axis=1), tf.argmax(Y_test, axis=1))
+    print('comm_round: {} | global_acc: {:.3%} | global_loss: {}'.format(comm_round, acc, loss))
+    return acc, loss
+
+# %%
+
+# Federated model training
+
+#initialize global model
+smlp_global = SimpleMLP()
+global_model = smlp_global.build(784, 10)
+        
+#commence global training loop
+for comm_round in range(comms_round):
+            
+    # get the global model's weights - will serve as the initial weights for all local models
+    global_weights = global_model.get_weights()
+    
+    #initial list to collect local model weights after scalling
+    scaled_local_weight_list = list()
+
+    #randomize client data - using keys
+    client_names= list(clients_batched.keys())
+    random.shuffle(client_names)
+    
+    #loop through each client and create new local model
+    for client in client_names:
+        smlp_local = SimpleMLP()
+        local_model = smlp_local.build(784, 10)
+        local_model.compile(loss=loss, 
+                      optimizer=optimizer, 
+                      metrics=metrics)
+        
+        #set local model weight to the weight of the global model
+        local_model.set_weights(global_weights)
+        
+        #fit local model with client's data
+        local_model.fit(clients_batched[client], epochs=1, verbose=0)
+        
+        #scale the model weights and add to list
+        scaling_factor = weight_scalling_factor(clients_batched, client)
+        scaled_weights = scale_model_weights(local_model.get_weights(), scaling_factor)
+        scaled_local_weight_list.append(scaled_weights)
+        
+        #clear session to free memory after each communication round
+        K.clear_session()
+        
+    #to get the average over all the local model, we simply take the sum of the scaled weights
+    average_weights = sum_scaled_weights(scaled_local_weight_list)
+    
+    #update global model 
+    global_model.set_weights(average_weights)
+
+    #test global model and print out metrics after each communications round
+    for(X_test, Y_test) in test_batched:
+        global_acc, global_loss = test_model(X_test, Y_test, global_model, comm_round)
+
+# %%
